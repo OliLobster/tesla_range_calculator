977034fdbdff21a6ac557f0191289698e6577a06
OliLobster <sgyr1993@163.com>
2023-03-28 00:01:39 +0800

    import TeslaNotice into TeslaBattery.js

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './TeslaBattery.css';
+import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
 class TeslaBattery extends React.Component {
     render() {
         return (
             <form className="tesla-battery">
                 <h1>Range Per Charge</h1>
+                <TeslaNotice />
             </form>
         )
     }
