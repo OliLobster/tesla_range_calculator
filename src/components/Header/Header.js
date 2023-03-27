6cb1ca683f7082de40e9e145f043d699a6360a7f
OliLobster <sgyr1993@163.com>
2023-03-27 22:46:17 +0800

    Update Header.js

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../assets/logo.svg';
+const Header = () => (
+    <div className="header">
+        <img src={logo} alt="Tesla" />
+    </div>
+)
+export default Header;
