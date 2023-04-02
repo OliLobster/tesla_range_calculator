a12e0c0de6aa77334b8c0e454b82aaae523d915b
OliLobster <sgyr1993@163.com>
2023-04-02 21:00:05 +0800

    install redux and react-redux

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import TeslaBattery from './containers/TeslaBattery';
-
+import {counterDefaultVal} from "./constants/counterDefaultVal";
 class App extends Component {
     render() {
         return (
